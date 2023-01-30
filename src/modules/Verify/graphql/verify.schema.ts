beae7ccd9c82cc2604495c5e6329bff81205f052
ZauJulio <zaujulio.dev@gmail.com>
2023-01-30 04:55:19 -0300

    docs: improve verify graphql schema

--- src/modules/Verify/graphql/verify.schema.ts
+++ src/modules/Verify/graphql/verify.schema.ts
@@ -2,17 +2,26 @@ import gql from 'graphql-tag';
 
 export const VerifySchema = gql`
   input Rule {
+    "The rule to check"
     rule: String
+    "The value to check against"
     value: Int
   }
 
   type VerifyResult {
+    "password is valid"
     verify: Boolean
+    "password breaks the rules"
     noMatch: [String]
   }
 
+  "The root query object"
   type Query {
-    hello: String
-    verify(password: String, rules: [Rule]): VerifyResult
+    verify(
+      "The password to verify"
+      password: String
+      "The rules for checking the password"
+      rules: [Rule]
+    ): VerifyResult
   }
 `;
