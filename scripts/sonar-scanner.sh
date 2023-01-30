ee90cb297bcd6beaae8e8b4560d2fa584eff0fd3
ZauJulio <zaujulio.dev@gmail.com>
2023-01-30 04:42:14 -0300

    fix: add shebang to code scanner script

--- scripts/sonar-scanner.sh
+++ scripts/sonar-scanner.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 PROJECT_NAME="StudioSolChallengeBackend"
 
 sonar-scanner \
   -Dsonar.projectName=$PROJECT_NAME \
   -Dsonar.projectKey=StudioSolChallengeBackend \
   -Dsonar.sources=. \
-  -Dsonar.host.url=http://0.0.0.0:9000 \
+  -Dsonar.host.url=http://0.0.0.0:9090 \
   -Dsonar.login=$STUDIOSOL_CHALLENGE_BACKEND
